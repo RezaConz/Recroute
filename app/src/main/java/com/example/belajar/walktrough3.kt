ddf74957f9fd75825db9834936e68878f116e3a4
rofif <rofifrahmanfuadi04@gmail.com>
2023-03-24 20:12:24 +0700

    perubahan baru

--- app/src/main/java/com/example/belajar/walktrough3.kt
+++ app/src/main/java/com/example/belajar/walktrough3.kt
@@ -1,11 +1,19 @@
 package com.example.belajar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class walktrough3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_walktrough3)
+
+        val buttonWt3 = findViewById<Button>(R.id.buttonWt3)
+        buttonWt3.setOnClickListener {
+            val intent = Intent (this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
