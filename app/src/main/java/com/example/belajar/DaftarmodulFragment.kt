0f92c578f9afe9287a7a5abf9d134c2cd248016d
reza <rezakurnia1902@gmail.com>
2023-04-06 15:35:42 +0700

    fitur youtube player

--- app/src/main/java/com/example/belajar/DaftarmodulFragment.kt
+++ app/src/main/java/com/example/belajar/DaftarmodulFragment.kt
@@ -5,55 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.belajar.databinding.FragmentDaftarmodulBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class DaftarmodulFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DaftarmodulFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class DaftarmodulFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentDaftarmodulBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daftarmodul, container, false)
+        binding = FragmentDaftarmodulBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DaftarmodulFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DaftarmodulFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.userinterfaceButton.setOnClickListener(this)
+        binding.userinterfaceButton.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.userinterface_button) {
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.beginTransaction().apply {
+                replace(
+                    R.id.frame_layout,
+                    UserinterfacedesignFragment(),
+                    UserinterfacedesignFragment::class.java.simpleName
+                )
+                addToBackStack(null)
+                commit()
+            }
+        }else if (v.id == R.id.backMyCourse) {
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.beginTransaction().apply {
+                replace(
+                    R.id.frame_layout,
+                    CourseFragment(),
+                    CourseFragment::class.java.simpleName
+                )
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+
 }
