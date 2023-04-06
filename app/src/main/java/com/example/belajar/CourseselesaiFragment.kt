578708aff1220adf939eb1d6e2956de023a89a5e
reza <rezakurnia1902@gmail.com>
2023-04-06 08:08:15 +0700

    menambahkan daftar modul

--- app/src/main/java/com/example/belajar/CourseselesaiFragment.kt
+++ app/src/main/java/com/example/belajar/CourseselesaiFragment.kt
@@ -6,58 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.belajar.databinding.FragmentCourseBinding
+import com.example.belajar.databinding.FragmentCourseselesaiBinding
 
-class CourseFragment : Fragment(), View.OnClickListener {
 
+class CourseselesaiFragment : Fragment(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    private lateinit var binding: FragmentCourseBinding
+    private lateinit var binding: FragmentCourseselesaiBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentCourseBinding.inflate(inflater,container,false)
+        binding = FragmentCourseselesaiBinding.inflate(inflater,container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.courseAktif1.setOnClickListener(this)
-        binding.courseSelesaiButton.setOnClickListener(this)
-        binding.notifCourseAktif.setOnClickListener(this)
+        binding.courseAktifButton.setOnClickListener(this)
+        binding.notifCourseSelesai.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.courseAktif_1) {
+        if (v.id == R.id.course_aktif_button) {
             val mFragmentManager = parentFragmentManager
             mFragmentManager.beginTransaction().apply {
                 replace(
                     R.id.frame_layout,
-                    DaftarmodulFragment(),
-                    DaftarmodulFragment::class.java.simpleName
+                    CourseFragment(),
+                    CourseFragment::class.java.simpleName
                 )
                 addToBackStack(null)
                 commit()
             }
-        }else if (v.id == R.id.course_selesai_button) {
-            val mFragmentManager = parentFragmentManager
-            mFragmentManager.beginTransaction().apply {
-                replace(
-                    R.id.frame_layout,
-                    CourseselesaiFragment(),
-                    CourseselesaiFragment::class.java.simpleName
-                )
-                addToBackStack(null)
-                commit()
-            }
-        }else if (v.id == R.id.notif_course_aktif) {
+        }else if (v.id == R.id.notif_course_selesai) {
             val mFragmentManager = parentFragmentManager
             mFragmentManager.beginTransaction().apply {
                 replace(
@@ -70,6 +57,5 @@ class CourseFragment : Fragment(), View.OnClickListener {
             }
         }
     }
-
 
 }
